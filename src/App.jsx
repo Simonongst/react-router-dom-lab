e2bb8af12110f2a50f12449ea708d907c2dcd74e
simonongst@gmail.com <simonongst@Simons-Mac-mini.local>
2025-09-17 23:31:50 +0800

    feat: create addBox function

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,28 @@ import MailboxList from './components/MailboxList/MailboxList';
 import MailboxForm from './components/MailboxForm/MailboxForm';
 import MailboxDetails from './components/MailboxDetails/MailboxDetails';
 import {Routes, Route} from 'react-router-dom'
+import {useState} from 'react'
 
 // src/App.jsx
 
 const App = () => {
+  const {mailboxes, setMailboxes} = useState([]);
+
+  const addBox = (formData) => {
+    const mailbox = {
+      _id: mailboxes.length + 1,
+      ...formData,
+    }
+    setMailboxes([...mailboxes, mailbox]);
+  }
+
   return (
     <>
       <NavBar />
       <Routes>
           <Route path='/' element={<main><h1>Post Office</h1></main>}/>
           <Route path='/mailboxes' element={<main><h1><MailboxList /></h1></main>}/>
-          <Route path='/new-mailbox' element={<main><h1><MailboxForm /></h1></main>}/>
+          <Route path='/new-mailbox' element={<main><h1><MailboxForm addBox={addBox}/></h1></main>}/>
           <Route path='/mailboxes/:mailboxId' element={<main><h1><MailboxDetails /></h1></main>}/>
           <Route path='*' element={<main><h1>Oops! We couldn’t find that page</h1></main>}/>
       </Routes>
